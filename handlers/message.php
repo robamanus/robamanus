34449e3c2350ad4df05b15541948edae7e453912
Rob <uniform86@mail.ru>
2021-04-18 18:42:29 +0200

    Deleted lib and handlers folders

--- handlers/message.php
+++
@@ -1,4 +0,0 @@
-<?php
-	echo "<div style='margin-top:75px;'><h1 style='font-family:Tahoma;font-size:18px;'>Загадайте двухзначное число и нажмите ОК</h1></div>
-		<div style='position:absolute;bottom:75px;left:31%;'><button style='width:200px;height:50px;font-family:Tahoma;font-size:18px;' id='ok'>ОК</button></div>";
-?>
